74a6dd8399dfce0696699762ae2aa1e158bcf897
tyronepost <tyrone.post.a@gmail.com>
2017-07-19 14:34:33 -0400

    implemented web interface for following and unfollowing users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -50,18 +50,28 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to root_url
   end
 
-  test 'should redirect destroy when not logged in' do
+  test "should redirect destroy when not logged in" do
     assert_no_difference 'User.count' do
       delete :destroy, id: @user
     end
     assert_redirected_to login_url
   end
 
-  test 'should redirect destroy when logged in as a non-admin' do
+  test "should redirect destroy when logged in as a non-admin" do
     log_in_as(@other_user)
     assert_no_difference 'User.count' do
       delete :destroy, id: @user
     end
     assert_redirected_to root_url
+  end
+
+  test "should redirect following when not logged in" do
+    get :following, id: @user
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get :followers, id: @user
+    assert_redirected_to login_url
   end
 end
